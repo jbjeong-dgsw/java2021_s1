b3d4f6ebf4de7f85ff080afabeb0cacabcc9aedc
정재봉 <jbjeong@dgsw.hs.kr>
2021-03-23 14:06:58 +0900

    3월 23일 3반 수업

---
+++ dgsw_3/src/kr/hs/dgsw/java/c3/variable/IntegerStudy.java
@@ -0,0 +1,32 @@
+package kr.hs.dgsw.java.c3.variable;
+
+public class IntegerStudy {
+
+	public static void main(String[] args) {
+		
+		byte byteValue = 1;
+		short shortValue = 1;
+		char charValue = 1;
+		int intValue = 1;
+		long longValue = 1;
+		
+		System.out.println("Byte : " + Byte.MAX_VALUE);
+		System.out.println("Size of Byte : " + Byte.SIZE);
+
+		System.out.println("Short : " + Short.MAX_VALUE);
+		System.out.println("Size of Short : " + Short.SIZE);
+
+		System.out.println("Int : " + Integer.MAX_VALUE);
+		System.out.println("Size of Int : " + Integer.SIZE);
+
+		System.out.println("Long : " + Long.MAX_VALUE);
+		System.out.println("Size of Long : " + Long.SIZE);
+
+		System.out.println("Char : " + (Character.MAX_VALUE + 0));
+		System.out.println("Size of Char : " + Character.SIZE);
+
+		byteValue = (byte)128;
+		System.out.println(byteValue);
+	}
+	
+}
---
+++ dgsw_3/src/kr/hs/dgsw/java/c3/variable/VariableStudy.java
@@ -0,0 +1,32 @@
+package kr.hs.dgsw.java.c3.variable;
+
+public class VariableStudy {
+
+	static final int SIZE = 3;
+
+	int age;
+
+	public void method(String nickname) {
+		String name = null;
+
+		if (null == name) {
+			int height = 182;
+		} else {
+			int height = 178;
+		}
+
+		int height = 160;
+		
+		System.out.println(height);
+	}
+
+	public static void main(String[] args) {
+		VariableStudy study1 = new VariableStudy();
+		VariableStudy study2 = new VariableStudy();
+
+		VariableStudy.age = 12;
+		VariableStudy.age = 18;
+
+		System.out.println(VariableStudy.age);
+	}
+}
