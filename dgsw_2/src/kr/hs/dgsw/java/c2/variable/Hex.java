57016896e284f6ba1c34177e2efebfdaa3cab1f8
정재봉 <jbjeong@dgsw.hs.kr>
2021-03-24 16:02:54 +0900

    3월 24일 2반 수업

--- dgsw_2/src/kr/hs/dgsw/java/c2/variable/Hex.java
+++ dgsw_2/src/kr/hs/dgsw/java/c2/variable/Hex.java
@@ -6,18 +6,50 @@ public class Hex {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int intValue = scanner.nextInt();
-		
+
 		String hex = convertToHexString(intValue);
 		System.out.println(intValue + " -> " + hex);
-		
+
 		scanner.close();
 	}
-	
+
 	private static String convertToHexString(int value) {
-		String result = null;
+		String result = "";
 		
-		// TODO
+		while (value > 0)
+		{
+			int remainder = value % 16;
+			String char1 = convertToHexChar(remainder);
+			result = char1 + result;
+			
+			value /= 16;
+		}
 		
 		return result;
+	}
+
+	private static String convertToHexChar(int value) {
+		switch (value) {
+		case 10: {
+			return "A";
+		}
+		case 11: {
+			return "B";
+		}
+		case 12: {
+			return "C";
+		}
+		case 13: {
+			return "D";
+		}
+		case 14: {
+			return "E";
+		}
+		case 15: {
+			return "F";
+		}
+		default:
+			return value + "";
+		}
 	}
 }
