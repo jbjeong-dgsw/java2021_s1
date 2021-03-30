827718d076ef554ff17d04384483a72ce2e40ac9
정재봉 <jbjeong@dgsw.hs.kr>
2021-03-30 12:21:41 +0900

    3월 30일 1반 수업

--- dgsw_1/src/kr/hs/dgsw/java/c1/inherit/Adder.java
+++ dgsw_1/src/kr/hs/dgsw/java/c1/inherit/Adder.java
@@ -1,37 +1,65 @@
 package kr.hs.dgsw.java.c1.inherit;
 
+import java.util.Scanner;
+
 public class Adder {
 	protected int operand1;
 	protected int operand2;
+	protected Scanner scanner;
+
+	public Adder() {
+		makeScanner();
+	}
+
+	public void execute() {
+		while (true) {
+			input();
+			
+			if (operand1 == 0 && operand2 == 0)
+			{
+				break;
+			}
+			
+			print();
+		}
+		
+		System.out.println("프로그램을 종료합니다.");
+		scanner.close();
+	}
 
 	public void setOperand1(int operand1) {
 		this.operand1 = operand1;
 	}
-	
+
 	public void setOperand2(int operand2) {
 		this.operand2 = operand2;
 	}
-	
+
 	public int calculate() {
 		return operand1 + operand2;
 	}
-	
+
 	public String getOperator() {
 		return "+";
 	}
-	
-	public void print()
-	{
-		String str = String.format("%d %s %d = %d", 
-				operand1, getOperator(), operand2, 
-				calculate());
+
+	public void print() {
+		String str = String.format("%d %s %d = %d", operand1, getOperator(), operand2, calculate());
 		System.out.println(str);
 	}
-	
+
+	public void input() {
+		System.out.println("두 수를 입력하세요.");
+		operand1 = scanner.nextInt();
+		operand2 = scanner.nextInt();
+	}
+
+	public void makeScanner() {
+		scanner = new Scanner(System.in);
+	}
+
 	public static void main(String[] args) {
 		Adder adder = new Adder();
-		adder.setOperand1(1239);
-		adder.setOperand2(5321);
-		adder.print();
+		adder.execute();
 	}
 }
--- dgsw_1/src/kr/hs/dgsw/java/c1/inherit/Subtractor.java
+++ dgsw_1/src/kr/hs/dgsw/java/c1/inherit/Subtractor.java
@@ -11,4 +11,9 @@ public class Subtractor extends Adder {
 	public String getOperator() {
 		return "-";
 	}
+	
+	public static void main(String[] args) {
+		Subtractor subtractor = new Subtractor();
+		subtractor.execute();
+	}
 }
