2d95eeadf58aff2a4e77ee2862efa4c5cb96ac1b
정재봉 <jbjeong@dgsw.hs.kr>
2021-03-30 15:10:01 +0900

    ..

--- dgsw_3/src/kr/hs/dgsw/java/c3/inherit/Adder.java
+++ dgsw_3/src/kr/hs/dgsw/java/c3/inherit/Adder.java
@@ -1,11 +1,30 @@
 package kr.hs.dgsw.java.c3.inherit;
 
+import java.util.Scanner;
+
 public class Adder {
 	protected int operand1;
 	protected int operand2;
+	protected Scanner scanner;
 
 	public void execute() {
-		// TODO
+		makeScanner();
+		
+		while (true) {
+			inputNumbers();
+			
+			// 만약 두 수가 0이라면 나간다
+			if (checkTerminate()) 
+			{
+				break;
+			}
+			
+			print();
+		}
+
+		closeScanner();
+
+		System.out.println("프로그램 종료");
 	}
 	
 	public int calculate() {
@@ -23,6 +42,24 @@ public class Adder {
 		System.out.println(str);
 	}
 
+	protected void makeScanner() {
+		scanner = new Scanner(System.in); 
+	}
+	
+	protected void closeScanner() {
+		scanner.close();
+	}
+	
+	protected void inputNumbers() {
+		System.out.println("두 정수를 입력하세요.");
+		operand1 = scanner.nextInt();
+		operand2 = scanner.nextInt();
+	}
+	
+	protected boolean checkTerminate() {
+		return ((0 == operand1) && (0 == operand2));
+	}
+	
 	public int getOperand1() {
 		return operand1;
 	}
