d1449cd4e9b626952fe99e4053136239ee5fb536
정재봉 <jbjeong@dgsw.hs.kr>
2021-04-14 16:02:45 +0900

    ..

---
+++ dgsw_2/src/kr/hs/dgsw/java/c2/array/Queue.java
@@ -0,0 +1,40 @@
+package kr.hs.dgsw.java.c2.array;
+
+public class Queue {
+	private static final int SIZE = 10;
+	
+	private String[] array = new String[SIZE];
+	
+	private int head = 0;
+
+	private int tail = 0;
+	
+	public void enqueue(String value) {
+		if ((head - tail) > SIZE) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		
+		array[head % SIZE] = value;
+		head++;
+	}
+	
+	public String dequeue() {
+		if (head <= tail) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		
+		String value = array[tail];
+		tail++;
+		
+		return value;
+	}
+	
+	public static void main(String[] args) {
+		Queue queue = new Queue();
+		
+		queue.enqueue("어벤져스");
+		queue.enqueue("인터스텔라");
+		
+		System.out.println(queue.dequeue());
+	}
+}
---
+++ dgsw_2/src/kr/hs/dgsw/java/c2/array/Stack.java
@@ -0,0 +1,28 @@
+package kr.hs.dgsw.java.c2.array;
+
+public class Stack {
+	private static final int SIZE = 10;
+	
+	private String[] array = new String[SIZE];
+	
+	private int cursor = 0;
+	
+	public void push(String value) {
+		array[cursor] = value;
+		cursor++;
+	}
+	
+	public String pop() {
+		cursor--;
+		return array[cursor];
+	}
+	
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		
+		stack.push("어벤져스");
+		stack.push("인터스텔라");
+		
+		System.out.println(stack.pop());
+	}
+}
