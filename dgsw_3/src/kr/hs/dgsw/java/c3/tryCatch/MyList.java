1522a13f7f0bb793d022cbd04cb4d3061ee7138b
정재봉 <jbjeong@dgsw.hs.kr>
2021-04-20 16:09:38 +0900

    ..

--- dgsw_3/src/kr/hs/dgsw/java/c3/tryCatch/MyList.java
+++ dgsw_3/src/kr/hs/dgsw/java/c3/tryCatch/MyList.java
@@ -8,28 +8,83 @@ public class MyList {
 	 * 새로운 값을 추가한다.
 	 */
 	public void add(String value) {
+		Node node = new Node();
+		node.setValue(value);
 		
+		if (head == null) {
+			head = node;
+		} else {
+			Node pointer = head;
+			
+			while (pointer.getNext() != null) {
+				pointer = pointer.getNext();
+			}
+			
+			pointer.setNext(node);
+		}
 	}
 	
 	/**
 	 * 주어진 위치의 값을 리턴한다.
 	 */
 	public String get(int index) {
-		return null;
+		return getNode(index).getValue();
 	}
 	
 	/**
 	 * 주어진 위치의 값을 삭제한다.
 	 */
 	public void remove(int index) {
-		
+		if (index > 0) {
+			Node prev = getNode(index - 1);
+			Node node = prev.getNext();
+			
+			if (node == null) {
+				throw new ArithmeticException();
+			}
+			
+			prev.setNext(node.getNext());
+		} else {
+			if (head == null) {
+				throw new ArithmeticException();
+			}
+			head = head.getNext();
+		}
 	}
 	
 	/**
 	 * 등록된 값들의 개수를 리턴한다.
 	 */
 	public int getSize() {
-		return 0;
+		Node pointer = head;
+		
+		int count = 0;
+		while (pointer != null) {
+			count++;
+			pointer = pointer.getNext();
+		}
+		
+		return count;
+	}
+	
+	private Node getNode(int index) {
+		if (index < 0) {
+			throw new IllegalArgumentException();
+		}
+		
+		Node pointer = head;
+		for (int i = 0 ; i < index ; i++) {
+			if (pointer == null) {
+				throw new ArrayIndexOutOfBoundsException();
+			}
+			
+			pointer = pointer.getNext();
+		}
+		
+		if (pointer == null) {
+			throw new ArrayIndexOutOfBoundsException();
+		}
+		return pointer;
 	}
 	
 	public static void main(String[] args) {
