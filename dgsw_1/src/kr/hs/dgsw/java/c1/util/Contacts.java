d371e1f7047985721520b628c12a8ac522942edb
정재봉 <jbjeong@dgsw.hs.kr>
2021-04-27 12:26:23 +0900

    ..

--- dgsw_1/src/kr/hs/dgsw/java/c1/util/Contacts.java
+++ dgsw_1/src/kr/hs/dgsw/java/c1/util/Contacts.java
@@ -1,29 +1,55 @@
 package kr.hs.dgsw.java.c1.util;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Contacts {
 
+	private Map<String, NameCard> map = 
+			new HashMap<String, NameCard>();
+	
+
 	public NameCard read(String name) {
-		return null;
+		return map.get(name);
 	}
 	
 	public void save(String name, String phone) {
+		NameCard card = new NameCard();
+		card.setName(name);
+		card.setPhone(phone);
 		
+		save(card);
 	}
 
 	public void save(NameCard card) {
-		
+		map.put(card.getName(), card);
 	}
 
 	public NameCard readByPhone(String phone) {
+		Collection<NameCard> list =
+				map.values();
+		
+		for (NameCard card : list) {
+			if (card.getPhone().equals(phone)) {
+				return card;
+			}
+		}
+		
 		return null;
 	}
 	
 	public int getSize() {
-		return 0;
+		return map.size();
 	}
 	
 	public void printAll() {
-		// 등록된 데이터의 이름과 전화번호를 sysout로 출력한다.
+		Collection<NameCard> list =
+				map.values();
+		
+		for (NameCard card : list) {
+			System.out.println(card.getName() + " - " + card.getPhone());
+		}
 	}
 	
 }
