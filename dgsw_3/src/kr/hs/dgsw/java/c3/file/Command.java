eafebd766e157ffd8511878526d8ee6b1f6f7396
정재봉 <jbjeong@dgsw.hs.kr>
2021-05-11 14:45:26 +0900

    ..

--- dgsw_3/src/kr/hs/dgsw/java/c3/file/Command.java
+++ dgsw_3/src/kr/hs/dgsw/java/c3/file/Command.java
@@ -1,16 +1,25 @@
 package kr.hs.dgsw.java.c3.file;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Command {
 
 	protected File currentDirectory;
 	
-	protected String line;
+	protected String[] tokens;
 	
-	public Command(File currentDirectory, String line) {
+	protected String word;
+	
+	protected String option;
+	
+	protected List<String> args;
+	
+	public Command(File currentDirectory, String[] tokens) {
 		this.currentDirectory = currentDirectory;
-		this.line = line;
+		this.tokens = tokens;
+		analizeLine();
 	}
 
 	public void execute() {
@@ -18,17 +27,33 @@ public abstract class Command {
 	}
 	
 	public boolean isExit() {
-		return "exit".equals(line);
+		return false;
+	}
+
+	private void analizeLine() {
+		word = tokens[0];
+		
+		args = new ArrayList<String>();
+		
+		for (int i = 1 ; i < tokens.length ; i++) {
+			if (tokens[i].startsWith("-")) {
+				option = tokens[i];
+			}
+			else {
+				args.add(tokens[i]);
+			}
+		}
 	}
 	
 	public static Command build(File currentDirectory, String line) {
 		Command command = null;
+		String[] tokens = line.split(" ");
 		
-		if ("exit".equals(line)) {
-			
+		if ("exit".equals(tokens[0])) {
+			command = new CommandExit(currentDirectory, tokens);
 		}
-		else if ("ls".equals(line)) {
-			command = new CommandLs(currentDirectory, line);
+		else if ("ls".equals(tokens[0])) {
+			command = new CommandLs(currentDirectory, tokens);
 		}
 		else {
 			throw new RuntimeException("잘못된 명령어입니다.");
--- dgsw_3/src/kr/hs/dgsw/java/c3/file/CommandLs.java
+++ dgsw_3/src/kr/hs/dgsw/java/c3/file/CommandLs.java
@@ -4,13 +4,35 @@ import java.io.File;
 
 public class CommandLs extends Command {
 
-	public CommandLs(File currentDirectory, String line) {
-		super(currentDirectory, line);
+	public CommandLs(File currentDirectory, String[] tokens) {
+		super(currentDirectory, tokens);
 	}
 	
 	@Override
 	public void execute() {
-		File[] files = currentDirectory.listFiles();
+		File dir = null;
+		
+		if (args.size() > 0) {
+			String dirName = args.get(0);
+			
+			if (dirName.startsWith("/")) {
+				dir = new File(dirName);
+			}
+			else if (dirName.equals("..")) {
+				dir = currentDirectory.getParentFile();
+			} 
+			else if (dirName.equals(".")) {
+				dir = currentDirectory;
+			}
+			else {
+				dir = new File(currentDirectory, dirName);
+			}
+		}
+		else {
+			dir = currentDirectory;
+		}
+		
+		File[] files = dir.listFiles();
 		
 		for (File file : files) {
 			showLine(file);
