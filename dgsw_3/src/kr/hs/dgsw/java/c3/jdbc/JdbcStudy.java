693a3b36776b3e53595891c3095f0b8642483654
정재봉 <jbjeong@dgsw.hs.kr>
2021-05-18 14:08:45 +0900

    ..

--- dgsw_3/src/kr/hs/dgsw/java/c3/jdbc/JdbcStudy.java
+++ dgsw_3/src/kr/hs/dgsw/java/c3/jdbc/JdbcStudy.java
@@ -7,58 +7,93 @@ import java.sql.ResultSet;
 
 public class JdbcStudy {
 
-	public void readList() throws Exception {
-		// 데이터베이스에 접속한다.
+	private Connection getConnection() throws Exception {
 		Class.forName("org.mariadb.jdbc.Driver");
 		Connection connection = 
 				DriverManager.getConnection(
 						"jdbc:mariadb://211.53.209.159/dgsw_java", 
-						"dgsw_student", 
+						"dgsw_student",
 						"1234");
 		
+		return connection;
+	}
+	
+	public void readList() throws Exception {
+		// 데이터베이스에 접속한다.
+		Connection connection = getConnection();
+
 		// 데이터를 읽어온다.
 		String sql = "SELECT * FROM phone_book ";
-		PreparedStatement pstmt = 
-				connection.prepareStatement(sql);
-		
+		PreparedStatement pstmt = connection.prepareStatement(sql);
+
 		ResultSet rs = pstmt.executeQuery();
-		
+
 		// 읽어온 데이터를 출력한다.
 		while (rs.next()) {
 			int id = rs.getInt("id");
 			String name = rs.getString("name");
 			String phoneNumber = rs.getString("phone_number");
 			String address = rs.getString("address");
-			
-			String str = String.format(
-					"%d : %s %s %s", 
-					id, name, phoneNumber, address);
+
+			String str = String.format("%d : %s %s %s", id, name, phoneNumber, address);
 			System.out.println(str);
 		}
 		rs.close();
 		pstmt.close();
-		
+
 		// 접속을 종료한다.
 		connection.close();
-		
+
 	}
-	
-	public NameCard read(int id) {
-		
-		return null;
-	}
-	
-	public static void main(String[] args) {
-		
-		try {
+
+	public NameCard read(int id) throws Exception {
+
+		// 데이터베이스에 접속한다.
+		Connection connection = getConnection();
+
+		// 데이터를 읽어온다.
+		String sql = "SELECT * FROM phone_book WHERE id = ? ";
+		PreparedStatement pstmt = 
+				connection.prepareStatement(sql);
+		pstmt.setInt(1, id);
+
+		ResultSet rs = pstmt.executeQuery();
+
+		// 읽어온 데이터를 출력한다.
+		NameCard nameCard = null;
+		while (rs.next()) {
+			int id1 = rs.getInt("id");
+			String name = rs.getString("name");
+			String phoneNumber = rs.getString("phone_number");
+			String address = rs.getString("address");
+
+			nameCard = new NameCard();
 			
+			nameCard.setId(id1);
+			nameCard.setName(name);
+			nameCard.setAddress(address);
+			nameCard.setPhoneNumber(phoneNumber);
+		}
+		rs.close();
+		pstmt.close();
+
+		// 접속을 종료한다.
+		connection.close();
+
+		return nameCard;
+	}
+
+	public static void main(String[] args) {
+
+		try {
+
 			JdbcStudy study = new JdbcStudy();
 			study.readList();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
